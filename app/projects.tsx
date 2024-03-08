d8c0979c244710915b5bcf915721bacfe85c3a11
Sibusiso <93.july.31@gmail.com>
2024-03-08 12:10:09 +0200

    changes to the projects

--- app/projects.tsx
+++ app/projects.tsx
@@ -3,7 +3,6 @@
 
 import NoteKeeperProject from "./Projects/note-keeper";
 import QuizProject from "./Projects/quiz-game";
-import WeatherProject from "./Projects/weather-app";
 import WineListProject from "./Projects/wine-list-app";
 
 const Projects = () => {
@@ -14,7 +13,6 @@ const Projects = () => {
         <div className="flex flex-wrap space-x-20">
           <NoteKeeperProject />
           <QuizProject />
-          <WeatherProject />
           <WineListProject />
         </div>
       </div>
